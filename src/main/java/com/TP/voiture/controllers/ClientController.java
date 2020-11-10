a2ecb0c72bb44ed857ace4e428dbcd330b986be7
Jean-François GAUTREAU <Jeff59550@gmail.com>
2020-11-10 09:11:47 +0100

    :sparkles: : add delete call

--- src/main/java/com/TP/voiture/controllers/ClientController.java
+++ src/main/java/com/TP/voiture/controllers/ClientController.java
@@ -3,6 +3,7 @@ package com.TP.voiture.controllers;
 import com.TP.voiture.models.Client;
 import com.TP.voiture.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping(path = "/clients")
 public class ClientController {
+
     @Autowired
     private ClientRepository clientRepository;
 
@@ -24,5 +26,10 @@ public class ClientController {
     @GetMapping("/{clientId}")
     public Optional<Client> getClient(@PathVariable("clientId") int clientId) {
         return clientRepository.findById(clientId);
+    }
+
+    @DeleteMapping("/delete/{clientId}")
+    public void deleteClient(@PathVariable("clientId") int clientId) {
+        clientRepository.deleteById(clientId);
     }
 }
